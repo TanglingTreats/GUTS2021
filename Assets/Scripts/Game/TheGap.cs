ca4efed5936ee6c608f67d3309ecb2adff70d438
Edwin <edwintang10@gmail.com>
2021-02-06 21:30:45 +0800

    Update scene and csv

--- Assets/Scripts/Game/TheGap.cs
+++ Assets/Scripts/Game/TheGap.cs
@@ -5,29 +5,51 @@ using UnityEngine;
 public class TheGap : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject topTri;
+    private System.Random rand = new System.Random();
+
+    private Transform P1;
+    private Transform P2;
 
     void Start()
     {
+        P1 = GameObject.Find("Player1 Tri").GetComponent<Transform>();
+        P2 = GameObject.Find("Player2 Tri").GetComponent<Transform>();
     }
 
 
-    void Move()
+    void Move(float speed)
     {
-        transform.Translate(-0.01f, 0, 0);
+        transform.Translate(-speed, 0, 0);
     }
 
-    public void Reset()
+    public void Reset(int yRange, int gapRange, int rotation)
     {
         // TODO: RNGesus y position
+        double y = -0.24 + yRange / 100.0 * (0.24 * 2);
 
-        transform.position = new Vector3(2, 0, 0);
+        double gap = gapRange / 100.0 * 0.1;
+
+        transform.Rotate((float)rotation * 180, 0, 0);
+
+        Debug.Log(this.transform.rotation.x);
+
+        if (this.transform.rotation.x % 1 == 0 && this.transform.rotation.x != 0)
+        {
+            P1.position = new Vector3(transform.position.x, -0.8f - (float)gap, 0);
+            P2.position = new Vector3(transform.position.x, 0.8f + (float)gap, 0);
+        }
+        else
+        {
+            P1.position = new Vector3(transform.position.x, 0.8f + (float)gap, 0);
+            P2.position = new Vector3(transform.position.x, -0.8f - (float)gap, 0);
+        }
+
+        transform.position = new Vector3(2, (float)y, 0);
     }
-
     // Update is called once per frame
     void Update()
     {
-        Move();
+        Move(0.2f);
     }
 
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,9 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameController : MonoBehaviour
 {
+
+    TheGap gap;
+    System.Random rand = new System.Random();
+    int level;
+    List<String[]> levels = new List<String[]>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        gap = GameObject.Find("TheGap").GetComponent<TheGap>();
+        level = rand.Next(99);
+
+        using (var reader = new StreamReader(Application.streamingAssetsPath + "/level.csv"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                levels.Add(values);
+            }
+        }
+    }
+
     [SerializeField] private uint chatLimit = 150;
     public uint charCount = 0;
 
@@ -15,5 +38,11 @@ public class GameController : MonoBehaviour
         {
             Debug.Log("Game Over.");
         }
+    }
+
+    public void TriggerReset()
+    {
+        gap.Reset(Int32.Parse(levels[level][0]), Int32.Parse(levels[level][1]), Int32.Parse(levels[level][2]));
+        level = (level + 1) % 100;
     }
 }
