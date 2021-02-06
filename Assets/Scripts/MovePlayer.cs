2c7fdbfb6fb19bf4cf01172ce8bf42efe10226c4
chalkfung <chalkfung@gmail.com>
2021-02-06 22:47:02 +0800

    test out quick and dirty lobby waiting

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Mirror;
  
@@ -7,6 +8,25 @@ public class MovePlayer : NetworkBehaviour
 {
     [SerializeField]
     private float speed;
+
+    void Start()
+    {
+        List<GameObject> playerGOs =  GameObject.FindGameObjectsWithTag("Player").ToList();
+        if (playerGOs.Count < 2)
+        {
+            foreach (var playerGO in playerGOs)
+            {
+                playerGO.GetComponent<MovePlayer>().enabled = false;
+            }
+        }
+        else
+        {
+            foreach (var playerGO in playerGOs)
+            {
+                playerGO.GetComponent<MovePlayer>().enabled = true;
+            }
+        }
+    }
     void OnEnable()
     {
         //gameObject.GetComponent<ChatBehaviour>().canvas.SetActive(false);
@@ -19,8 +39,12 @@ public class MovePlayer : NetworkBehaviour
             GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, 0.0f);
             if(Input.GetKeyDown(KeyCode.Q))
             {
-                gameObject.GetComponent<ChatBehaviour>().canvas.SetActive(true);
-                gameObject.GetComponent<MovePlayer>().enabled = false;
+                List<GameObject> playerGOs =  GameObject.FindGameObjectsWithTag("Player").ToList();
+                foreach (var playerGO in playerGOs)
+                {
+                    gameObject.GetComponent<ChatBehaviour>().canvas.SetActive(true);
+                    gameObject.GetComponent<MovePlayer>().enabled = false;
+                }
             }
         }
     }
