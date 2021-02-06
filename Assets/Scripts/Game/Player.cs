ad4d6c43f0a111268c069e66317a412b184c32af
Edwin <edwintang10@gmail.com>
2021-02-06 20:25:06 +0800

    Add rigidbody jump to player. Add timer reset when player touches floor.

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Timer is in seconds
+    public float timer;
+    private float currTime;
+    public float jumpVal;
+
+    // GameObjects
+    private GameObject player;
+    private Transform playerPos;
+    private Rigidbody2D playerBody;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        this.player = GameObject.Find("Player");
+        this.playerPos = player.GetComponent<Transform>();
+        this.playerBody = player.GetComponent<Rigidbody2D>();
 
+        this.currTime = this.timer;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetButton("Jump") && this.currTime > 0)
+        {
+            Jump(this.jumpVal);
+            DecreaseTimer(0.1f);
+        }
+    }
+
+    public void Jump(float val)
+    {
+        playerBody.velocity = new Vector3 (0, val, 0);
+    }
+    
+    public void DecreaseTimer(float step)
+    {
+        this.currTime -= step;
+    }
+
+    public void ResetTimer ()
+    {
+        this.currTime = this.timer;
     }
 
     public void Kill()
