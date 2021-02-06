74b11043d0f2ec12356662b7750403d0ef5974fe
catkane-doodles <tanpeikai@hotmail.com>
2021-02-06 16:07:09 +0800

    add prototype scene

---
+++ Assets/Scripts/Game/TheGap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheGap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject topTri;
+
+    void Start()
+    {
+    }
+
+
+    void Move()
+    {
+        transform.Translate(-0.01f, 0, 0);
+    }
+
+    public void Reset()
+    {
+        // TODO: RNGesus y position
+
+        transform.position = new Vector3(2, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+}
---
+++ Assets/Scripts/Game/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "TheGap")
+        {
+            collision.gameObject.GetComponent<TheGap>().Reset();
+        }
+    }
+}
